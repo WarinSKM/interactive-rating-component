ef2e0b9714fc935fc9e607f628f6bed9800a685c
warinSKM <warin.jark@gmail.com>
2022-11-23 21:48:44 +0700

    add alt to image and change h2 to h1

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ function App() {
         <>
           <div className="box-border max-w-xs md:max-w-md bg-gradient-radial from-[#232A34] to-[#181E27] rounded-2xl p-10">
             <div className="rounded-full bg-dark-blue w-12 h-12 flex justify-center items-center mb-4 md:mb-8">
-              <img src="/images/icon-star.svg" />
+              <img src="/images/icon-star.svg" alt="icon-star" />
             </div>
-            <h2 className="text-white text-2xl md:text-3xl mb-2 md:mb-4">How did we do?</h2>
+            <h1 className="text-white text-2xl md:text-3xl mb-2 md:mb-4">How did we do?</h1>
             <p className="text-light-grey text-sm md:text-base mb-6">
               Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
             </p>
@@ -37,12 +37,12 @@ function App() {
       ) : (
         <>
           <div className="box-border max-w-xs md:max-w-md bg-gradient-radial from-[#232A34] to-[#181E27] rounded-2xl p-10 flex flex-col justify-center items-center">
-            <img src="images/illustration-thank-you.svg" />
+            <img src="images/illustration-thank-you.svg" alt="thank-you-image"/>
             <div className="rounded-full bg-dark-blue flex justify-center items-center px-4 py-1 mt-4">
               <p className="text-primary text-sm">You selected {rating} of 5 </p>
             </div>
             <div className="flex flex-col justify-center items-center mt-5">
-              <h2 className="text-white text-2xl md:text-3xl mb-2">Thank you!</h2>
+              <h1 className="text-white text-2xl md:text-3xl mb-2">Thank you!</h1>
               <p className="text-light-grey text-center">
                 We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!
               </p>
